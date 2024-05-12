9b944e664086bd226fb81bc4e42c921029d802cd
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2024-05-12 03:00:28 -0600

    Set horizontal scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Scroll, ScrollControls } from "@react-three/drei";
 import Faces from "./components/Faces";
+import ScrollNavigation from "./components/ScrollNavigation";
 
 const App = () => {
   //const [backgroundColor, setBackgroundColor] = useState("orange");
@@ -9,7 +10,9 @@ const App = () => {
   // camera={{ fov: 45.0, zoom: 1, position: [0, 0, 50] }
   return (
     <Canvas>
-      <ScrollControls pages={4} damping={0.5}>
+      <ambientLight intensity={Math.PI * 0.5} />
+      <ScrollControls horizontal={true} pages={4} damping={0.2}>
+        <ScrollNavigation />
         <Scroll>
           <Faces />
         </Scroll>
