1c86ea85f48321fdd1c865d2ba6ebd74e8593c1d
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2024-05-12 01:15:22 -0600

    Add Faces.jsx

---
+++ src/components/Faces.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Faces = () => {
+  return (
+    <>
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial wireframe={true} />
+      </mesh>
+    </>
+  );
+};
+
+export default Faces;
