5f84e122672f2da388dc2deaac6391091d162bbf
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2024-05-12 02:59:53 -0600

    Update file

--- src/components/Faces.jsx
+++ src/components/Faces.jsx
@@ -1,13 +1,27 @@
+import { useScroll } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import React from "react";
+import { useRef } from "react";
 
-const Faces = () => {
+const Faces = (props) => {
+  const meshRef = useRef();
+  const scroll = useScroll();
+
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.scale.set(
+        state.size.width * 0.001,
+        state.size.width * 0.001,
+        state.size.width * 0.001
+      );
+    }
+  });
+
   return (
-    <>
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial wireframe={true} />
-      </mesh>
-    </>
+    <mesh {...props} ref={meshRef}>
+      <boxGeometry />
+      <meshStandardMaterial wireframe={true} color="white" />
+    </mesh>
   );
 };
 
