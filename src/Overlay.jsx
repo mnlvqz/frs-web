1ec7077395680aa72e9d3eb7f02047e98b12f1dc
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2024-06-11 02:18:35 -0600

    Update overlay

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -5,6 +5,8 @@ import {
   Flex,
   Grid,
   GridItem,
+  Heading,
+  Stack,
   Text,
   VStack,
   extendTheme,
@@ -21,25 +23,23 @@ const Overlay = () => {
     <ChakraProvider theme={theme}>
       <VStack spacing={0}>
         {/* Home */}
-        <Container maxW="100%" h="100vh" bg="teal">
+        <Container maxW="100%" h="100vh" padding={10} bg="teal">
           <Center h="100%">
             <Marquee />
           </Center>
         </Container>
 
         {/* Introduction */}
-        <Container maxW="100%" h="100vh" padding={50} bg="salmon">
-          <Center h="100%">
-            <Flex
-              h="100%"
-              direction="column"
-              align="left"
-              justify="center"
-              textAlign={{ base: "justify", lg: "justify" }}
-            >
-              <br />
+        <Container maxW="100%" h="100vh" padding={10} bg="salmon">
+          <Flex height="100%" alignItems="center" justifyContent="center">
+            <Stack gap={5}>
+              <Heading fontSize={{ base: "3xl", lg: "4xl" }} color="white">
+                ¿Qué son?
+              </Heading>
               <Text
-                fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
+                fontSize={{ base: "xl", lg: "2xl" }}
+                textAlign="justify"
+                width={{ base: "300px", lg: "400px" }}
                 color="white"
               >
                 Los Sistemas de Reconocimiento Facial (SRF) son una tecnología
@@ -48,122 +48,146 @@ const Overlay = () => {
                 bases de datos además de modelos preestablecidos, con el fin de
                 identificar o verificar la identidad de las personas.
               </Text>
-            </Flex>
-          </Center>
+            </Stack>
+          </Flex>
         </Container>
 
         {/* History */}
-        <Container maxW="100%" padding={10} bg="orange">
-          <Grid
-            templateColumns={{ base: "1fr", lg: "repeat(5, 1fr)" }}
-            templateRows={{ base: "repeat(2, r1fr)", lg: "repeat(1, r1fr)" }}
-            gap={5}
+        <Container
+          maxW="100%"
+          h={{ base: "none", lg: "100vh" }}
+          padding={10}
+          bg="orange"
+        >
+          <Flex
+            flexDirection={{ base: "column", lg: "row" }}
+            height="100%"
+            alignItems="center"
+            justifyContent="center"
+            gap={{ base: "10", lg: "20" }}
           >
-            <GridItem colSpan={2}>
-              <Center h="100%">
-                <Flex
-                  h="100%"
-                  direction="column"
-                  align="left"
-                  justify="center"
-                  textAlign={{ base: "justify", lg: "justify" }}
-                >
-                  <br />
-                  <Text
-                    fontSize={{ base: "xl", md: "xl", lg: "2xl" }}
-                    color="white"
-                    padding={10}
-                  >
-                    Los Sistemas de Reconocimiento Facial (SRF) son una
-                    tecnología biométrica que utiliza técnicas y algoritmos de
-                    visión computarizada para analizar rasgos faciales,
-                    comparándolos con bases de datos además de modelos
-                    preestablecidos, con el fin de identificar o verificar la
-                    identidad de las personas.
-                  </Text>
-                </Flex>
-              </Center>
-            </GridItem>
-            <GridItem colSpan={3}>
-              <Center h="100%">
-                <Grid
-                  templateColumns={{ base: "1fr", lg: "repeat(3, 1fr)" }}
-                  templateRows={{
-                    base: "repeat(1, 1fr)",
-                    lg: "repeat(2, 1fr)",
-                  }}
-                  gap={5}
-                  padding={10}
-                >
-                  <GridItem>
-                    <DataModal
-                      btnimage="./frs-web/assets/images/history/sixties/button_500.png"
-                      year="1960"
-                      data="Los inicios de los SRF se remontan a la década de 1960. 
+            <Stack gap={5}>
+              <Heading fontSize={{ base: "3xl", lg: "4xl" }} color="white">
+                ¿Cuál es su historia?
+              </Heading>
+              <Text
+                fontSize={{ base: "xl", lg: "2xl" }}
+                textAlign="justify"
+                width={{ base: "300px", lg: "400px" }}
+                color="white"
+              >
+                Los Sistemas de Reconocimiento Facial (SRF) son una tecnología
+                biométrica que utiliza técnicas y algoritmos de visión
+                computarizada para analizar rasgos faciales, comparándolos con
+                bases de datos además de modelos preestablecidos, con el fin de
+                identificar o verificar la identidad de las personas.
+              </Text>
+            </Stack>
+            <Grid
+              templateColumns={{ base: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
+              templateRows={{ base: "repeat(2, 1fr)", lg: "repeat(2, 1fr)" }}
+              width={{ base: "300px", lg: "800px" }}
+              gap={5}
+            >
+              <GridItem>
+                <DataModal
+                  previewImg="./frs-web/assets/images/history/sixties/button_500.png"
+                  backgroundImg={[
+                    "./frs-web/assets/images/history/sixties/figure_01.png",
+                    "./frs-web/assets/images/history/sixties/figure_02.png",
+                    "./frs-web/assets/images/history/sixties/figure_03.png",
+                  ]}
+                  year="1960"
+                  data="Los inicios de los SRF se remontan a la década de 1960. 
                   Producto de las investigaciones de Woody Bledsoe, Helen Chan Wolf y Charles Bisson 
                   se consiguió desarrollar el sistema RAND, este empleaba coordenadas introducidas por 
                   el usuario para calcular las distancias y proporciones entre los puntos de referencia y 
                   los rasgos faciales a identificar."
-                    />
-                  </GridItem>
-                  <GridItem>
-                    <DataModal
-                      btnimage="./frs-web/assets/images/history/seventies/button_500.png"
-                      year="1970"
-                      data="Para 1970, Jay Goldstein, Leon D. Harmon, y Ann B. Lesk lograron expandir el número 
+                />
+              </GridItem>
+              <GridItem>
+                <DataModal
+                  previewImg="./frs-web/assets/images/history/seventies/button_500.png"
+                  backgroundImg={[
+                    "./frs-web/assets/images/history/seventies/figure_01.png",
+                    "./frs-web/assets/images/history/seventies/figure_02.png",
+                    "./frs-web/assets/images/history/seventies/figure_03.png",
+                    "./frs-web/assets/images/history/seventies/figure_04.png",
+                  ]}
+                  year="1970"
+                  data="Para 1970, Jay Goldstein, Leon D. Harmon, y Ann B. Lesk lograron expandir el número 
                   de rasgos identificables, características como el color del cabello o el grosor de los labios 
                   ahora podían ser identificados.En 1977 Takeo Kanade publicó “Computer Recognition of Human Faces”, 
                   el primer libro sobre reconocimiento facial."
-                    />
-                  </GridItem>
-                  <GridItem>
-                    <DataModal
-                      btnimage="./frs-web/assets/images/history/eighties/button_500.png"
-                      year="1980"
-                      data="La década de 1980 representó un gran periodo de avance para el desarrollo
+                />
+              </GridItem>
+              <GridItem>
+                <DataModal
+                  previewImg="./frs-web/assets/images/history/eighties/button_500.png"
+                  backgroundImg={[
+                    "./frs-web/assets/images/history/eighties/figure_01.png",
+                    "./frs-web/assets/images/history/eighties/figure_02.png",
+                    "./frs-web/assets/images/history/eighties/figure_03.png",
+                    "./frs-web/assets/images/history/eighties/figure_04.png",
+                    "./frs-web/assets/images/history/eighties/figure_05.png",
+                  ]}
+                  year="1980"
+                  data="La década de 1980 representó un gran periodo de avance para el desarrollo
                   de esta tecnología gracias al exponencial desarrollo de las computadoras. Lawrence
                   Sirovich y Michael Kirby implementaron los principios del álgebra lineal para la
                   identificación de rostros mediante el análisis de componentes principales en 1988."
-                    />
-                  </GridItem>
-                  <GridItem>
-                    <DataModal
-                      btnimage="./frs-web/assets/images/history/nineties/button_500.png"
-                      year="1990"
-                      data="Este trabajo permitió que en 1991 Matthew Turk y Alex Pentland desarrollaran Eigenface, 
+                />
+              </GridItem>
+              <GridItem>
+                <DataModal
+                  previewImg="./frs-web/assets/images/history/nineties/button_500.png"
+                  backgroundImg={[
+                    "./frs-web/assets/images/history/nineties/figure_01.png",
+                    "./frs-web/assets/images/history/nineties/figure_02.png",
+                    "./frs-web/assets/images/history/nineties/figure_03.png",
+                    "./frs-web/assets/images/history/nineties/figure_04.png",
+                  ]}
+                  year="1990"
+                  data="Este trabajo permitió que en 1991 Matthew Turk y Alex Pentland desarrollaran Eigenface, 
                   un SRF basado en vectores que empleaba visión computarizada. En 1993 la Agencia de Proyectos de 
                   Investigación Avanzados de Defensa (DARPA) y el Instituto Nacional de Estándares y Tecnología (NIST) 
                   crearon el programa Tecnología para el Reconocimiento Facial (FERET) con el fin de establecer 
                   un mercado del comercial de los SRF."
-                    />
-                  </GridItem>
-                  <GridItem>
-                    <DataModal
-                      btnimage="./frs-web/assets/images/history/thousand/button_500.png"
-                      year="2000"
-                      data="2001 fue el año en que Paul Viola y Michael Jones desarrollaron el sistema para la 
+                />
+              </GridItem>
+              <GridItem>
+                <DataModal
+                  previewImg="./frs-web/assets/images/history/thousand/button_500.png"
+                  backgroundImg={[
+                    "./frs-web/assets/images/history/thousand/figure_01.png",
+                    "./frs-web/assets/images/history/thousand/figure_02.png",
+                  ]}
+                  year="2000"
+                  data="2001 fue el año en que Paul Viola y Michael Jones desarrollaron el sistema para la 
                   detección de objetos Viola-Jones enfocado en rostros. Este sistema, hacía uso de machine 
                   learning para detectar en tiempo real las características faciales."
-                    />
-                  </GridItem>
-                  <GridItem>
-                    <DataModal
-                      btnimage="./frs-web/assets/images/history/today/button_500.png"
-                      year="Hoy"
-                      data="A partir de este punto los SRF comenzaron a ser mucho
+                />
+              </GridItem>
+              <GridItem>
+                <DataModal
+                  previewImg="./frs-web/assets/images/history/today/button_500.png"
+                  backgroundImg={[
+                    "./frs-web/assets/images/history/today/figure_01.png",
+                    "./frs-web/assets/images/history/today/figure_02.png",
+                  ]}
+                  year="Hoy"
+                  data="A partir de este punto los SRF comenzaron a ser mucho
                   más eficientes y precisos en razón del uso de redes neuronales convolucionales e
                   inteligencia artificial a la par del creciente poder computacional de los sistemas en
                   los que son implementados."
-                    />
-                  </GridItem>
-                </Grid>
-              </Center>
-            </GridItem>
-          </Grid>
+                />
+              </GridItem>
+            </Grid>
+          </Flex>
         </Container>
 
         {/* Techniques */}
-        <Container maxW="100%" padding={10} bg="purple">
+        <Container maxW="100%" h="100vh" padding={10} bg="pink">
           <Grid
             templateColumns={{ base: "1fr", lg: "repeat(5, 1fr)" }}
             templateRows={{ base: "repeat(2, r1fr)", lg: "repeat(1, r1fr)" }}
@@ -182,7 +206,7 @@ const Overlay = () => {
                 >
                   <GridItem>
                     <DataModal
-                      btnimage="./frs-web/assets/images/techniques/holistic/button_500.png"
+                      previewImg="./frs-web/assets/images/techniques/holistic/button_500.png"
                       year="Locales, holísticos e híbridos"
                       data="Los métodos locales, como los patrones binarios locales, se enfocan en rasgos 
                       específicos del rostro para el reconocimiento. Por otro lado, los métodos holísticos, 
@@ -193,7 +217,7 @@ const Overlay = () => {
                   </GridItem>
                   <GridItem>
                     <DataModal
-                      btnimage="./frs-web/assets/images/techniques/appearance/button_500.png"
+                      previewImg="./frs-web/assets/images/techniques/appearance/button_500.png"
                       year="Modelos y apariencia"
                       data="Los métodos basados en la apariencia tratan una imagen como un punto en un espacio 
                       vectorial de alta dimensión y utilizan técnicas estadísticas, como PLS, para comparar la 
@@ -204,7 +228,7 @@ const Overlay = () => {
                   </GridItem>
                   <GridItem>
                     <DataModal
-                      btnimage="./frs-web/assets/images/techniques/geometry/button_500.png"
+                      previewImg="./frs-web/assets/images/techniques/geometry/button_500.png"
                       year="Geometría y plantillas"
                       data="Los métodos basados en la geometría, como la correspondencia elástica de grafos, examinan 
                       los rasgos faciales locales y sus relaciones geométricas. Los métodos basados en plantillas 
@@ -214,7 +238,7 @@ const Overlay = () => {
                   </GridItem>
                   <GridItem>
                     <DataModal
-                      btnimage="./frs-web/assets/images/techniques/neural/button_500.png"
+                      previewImg="./frs-web/assets/images/techniques/neural/button_500.png"
                       year="Correspondencias, estadísticos y redes neuronales"
                       data="Los métodos de  correspondencia de modelos representan patrones a través de modelos, píxeles, 
                       curvas o texturas. La función de reconocimiento generalmente se basa en una medida de distancia o 
@@ -255,7 +279,13 @@ const Overlay = () => {
 
         {/* Home */}
         <Container maxW="100%" h="100vh" padding={0}>
-          <Carousel />
+          <Grid
+            templateColumns={{ base: "1fr", lg: "repeat(5, 1fr)" }}
+            templateRows={{ base: "repeat(2, r1fr)", lg: "repeat(1, r1fr)" }}
+            gap={5}
+          >
+            TEST
+          </Grid>
         </Container>
       </VStack>
     </ChakraProvider>
