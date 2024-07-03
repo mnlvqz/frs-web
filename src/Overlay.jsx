8b6eaf7f2bbf1de998406b36489155b35c8c1fd3
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2024-07-03 12:33:51 -0600

    Update style

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -11,6 +11,7 @@ import {
   Spacer,
   Stack,
   Text,
+  Tooltip,
   VStack,
   Wrap,
   WrapItem,
@@ -19,6 +20,7 @@ import {
 import React from "react";
 import CarouselCard from "./components/CarouselCard";
 import "@fontsource/win95fa";
+import Source from "./components/Source";
 
 const theme = extendTheme({
   colors: {
@@ -141,8 +143,15 @@ const Overlay = () => {
           </Center>
         </Box>
         {/* Definition */}
-        <Box width="100%" height="calc(100vh / 3 * 2)">
-          <Stack height="100%" direction="row" spacing={0}>
+        <Box
+          width="100%"
+          height={{ md: "calc(100vh / 3 * 4)", lg: "calc(100vh / 3 * 2)" }}
+        >
+          <Stack
+            height="100%"
+            direction={{ base: "column", lg: "row" }}
+            spacing={0}
+          >
             <Box
               flex="2"
               height="100%"
@@ -153,10 +162,10 @@ const Overlay = () => {
             ></Box>
             <Box flex="1" height="100%" bg="timberwolf.500">
               <Center height="100%">
-                <VStack padding={10} spacing={5}>
+                <Stack padding={10} spacing={5}>
                   <Heading
                     width="100%"
-                    fontSize={{ base: "6xl", lg: "4xl" }}
+                    fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}
                     color="gunmetal.500"
                     textShadow="4px 4px 0px #4CAEBD"
                     textAlign="left"
@@ -167,26 +176,76 @@ const Overlay = () => {
                     width="100%"
                     textAlign="justify"
                     color="gunmetal.500"
-                    fontSize={{ base: "lg", lg: "2xl" }}
+                    fontSize={{ base: "md", md: "lg", lg: "xl" }}
                   >
                     Los Sistemas de Reconocimiento Facial (SRF) son una
                     tecnología biométrica que utiliza técnicas y algoritmos de
                     visión computarizada para analizar rasgos faciales,
                     comparándolos con bases de datos además de modelos
                     preestablecidos, con el fin de identificar o verificar la
-                    identidad de las personas
+                    identidad de las personas.
+                    <Source
+                      symbol="[1]"
+                      source="Srinivas, T. S., Goutham, T., & Kumaran3, S. (2022, Mar). Face Recognition based Smart Attendance System Using IoT. International Research Journal of Engineering and Technology (IRJET), 9(3), 182-186."
+                    />
                   </Text>
-                </VStack>
+                  <Text
+                    width="100%"
+                    textAlign="justify"
+                    color="gunmetal.500"
+                    fontSize={{ base: "md", md: "lg", lg: "xl" }}
+                  >
+                    ¿Sabías qué el Sistema de INTERPOL de reconocimiento facial
+                    (IFRS) cuenta con registros faciales de más de 179 países?
+                    <Source
+                      symbol="[2]"
+                      source="INTERPOL. (2024). Facial Recognition. https://www.interpol.int/How-we-work/Forensics/Facial-Recognition"
+                    />
+                  </Text>
+                </Stack>
               </Center>
             </Box>
           </Stack>
         </Box>
-        <Box width="100%" height="calc(100vh / 3 * 2)">
-          <Stack direction="row" width="100%" height="100%" spacing={0}>
+        <Box
+          width="100%"
+          height={{ md: "calc(100vh / 3 * 4)", lg: "calc(100vh / 3 * 2)" }}
+        >
+          <Stack
+            direction={{ base: "column", lg: "row" }}
+            width="100%"
+            height="100%"
+            spacing={0}
+          >
             {/* History */}
             <CarouselCard
               title="¿Cuál es su historia?"
               mainImage="/frs-web/assets/images/main/face-03.webp"
+              source={
+                <>
+                  <Source
+                    symbol="[3]"
+                    source="Thorat, S. B., Nayak, S. K., & Dandale, J. P. (2010). Facial Recognition Technology: An analysis with scope in India. International Journal of Computer Science and Information Security, 8(1)."
+                  />
+                  <Source
+                    symbol="[4]"
+                    source="Alam, A. (2020, Agosto). An Analytical Research on Face Recognition Attendance System for Employees using Machine Learning. NeuroQuantology, 18(6), 130-135. ResearchGate. 10.48047/nq.2020.18.8.NQ20215"
+                  />
+                  <Source
+                    symbol="[5]"
+                    source="NEC. (2023). A brief history of Facial Recognition. https://www.nec.co.nz/market-leadership/publications-media/a-brief-history-of-facial-recognition"
+                  />
+                  <Source
+                    symbol="[6]"
+                    source="Lydick, N. (s. f.). A Brief Overview of Facial Recognition. https://www.eecs.umich.edu/courses/eecs487/w07/sa/pdf/nlydick-facial-recognition.pdf
+"
+                  />
+                  <Source
+                    symbol="[7]"
+                    source="Kanade, T. (2013). Computer Recognition of Human Faces. Springer Basel AG."
+                  />
+                </>
+              }
               information={[
                 [
                   "1960",
@@ -224,6 +283,14 @@ const Overlay = () => {
             <CarouselCard
               title="¿Cómo funcionan?"
               mainImage="/frs-web/assets/images/main/face-04.webp"
+              source={
+                <>
+                  <Source
+                    symbol="[8]"
+                    source="Akhtar, Z & Rattani, A. (2017). A Face in any Form: New Challenges and Opportunities for Face Recognition Technology. Computer. 50. 80-90. 10.1109/MC.2017.119. "
+                  />
+                </>
+              }
               information={[
                 [
                   "Locales, holísticos e híbridos",
@@ -250,36 +317,67 @@ const Overlay = () => {
           </Stack>
         </Box>
         <Box width="100%" height="calc(100vh / 3 * 2)">
-          <Stack direction="row" width="100%" height="100%" spacing={0}>
+          <Stack
+            direction={{ base: "column", lg: "row" }}
+            width="100%"
+            height="100%"
+            spacing={0}
+          >
             {/* Applications */}
             <CarouselCard
               title="¿Para qué son utilizados?"
               mainImage="/frs-web/assets/images/main/face-06.webp"
+              source={
+                <>
+                  <Source
+                    symbol="[9]"
+                    source="Amazon Web Services, Inc. (2024). ¿Qué es el reconocimiento facial? - Explicación del software de reconocimiento facial y análisis facial. https://aws.amazon.com/es/what-is/facial-recognition/"
+                  />
+                  <Source
+                    symbol="[10]"
+                    source="Kaspersky Latam. Reconocimiento facial: definición y explicación. (2024). https://latam.kaspersky.com/resource-center/definitions/what-is-facial-recognition"
+                  />
+                  <Source
+                    symbol="[11]"
+                    source="Riley, D. (2021). Expert available: Applications of facial recognition technology. Milwaukee School of Engineering - MSOE. https://www.msoe.edu/about-msoe/news/details/expert-available-applications-of-facial-recognition-technology/
+"
+                  />
+                  <Source
+                    symbol="[12]"
+                    source="NEC. (2021). What is the future of facial recognition technology in 2022 and beyond?. https://www.nec.co.nz/market-leadership/publications-media/what-is-the-future-of-facial-recognition-technology-in-2022-and-beyond/"
+                  />
+                </>
+              }
               information={[
                 [
                   "Control de espacios",
                   "Los SRF se usan para gestionar el acceso a edificios y áreas restringidas, verificando identidades para mejorar la seguridad.",
-                  "",
+                  "/frs-web/assets/images/applications/0.png",
                 ],
                 [
                   "Gestión de datos biométricos",
                   "Recopilan y analizan características faciales para crear perfiles únicos, facilitando la administración de bases de datos de identificación y emisión de documentos.",
+                  "/frs-web/assets/images/applications/1.png",
                 ],
                 [
                   "Seguridad pública y vigilancia",
                   "Permiten  identificar personas de interés en tiempo real y monitorizar multitudes, ayudando a prevenir delitos.",
+                  "/frs-web/assets/images/applications/2.png",
                 ],
                 [
                   "Interfaces de usuario",
                   "Facilitan el desbloqueo de dispositivos y la autenticación de usuarios, proporcionando acceso rápido y seguro a dispositivos y aplicaciones.",
+                  "/frs-web/assets/images/applications/3.png",
                 ],
                 [
                   "Mercadotecnia ",
                   "Analizan características faciales para personalizar anuncios y promociones, ajustándose a las preferencias y estados de ánimo de los usuarios.",
+                  "/frs-web/assets/images/applications/4.png",
                 ],
                 [
                   "Robótica",
                   "Mejoran la interacción de robots y sistemas automatizados con humanos, permitiéndoles realizar tareas de manera más eficiente en diversos entornos.",
+                  "/frs-web/assets/images/applications/5.png",
                 ],
               ]}
             />
@@ -287,48 +385,86 @@ const Overlay = () => {
             <CarouselCard
               title="¿Cuáles son sus implicaciones?"
               mainImage="/frs-web/assets/images/main/face-05.webp"
+              source={
+                <>
+                  <Source
+                    symbol="[13]"
+                    source="Andrejevic, M., & Selwyn, N. (2019). Facial recognition technology in schools: critical questions and concerns. Learning, Media & Technology/Learning, Media and Technology, 45(2), 115–128. https://doi.org/10.1080/17439884.2020.1686014"
+                  />
+                  <Source
+                    symbol="[14]"
+                    source="Irvine, M., & Turner, J. (2021). Toward Ethical Applications of Artificial intelligence: Understanding current uses of facial recognition technology and advancing bias mitigation strategies. DigitalGeorgetown. http://hdl.handle.net/10822/1062313 "
+                  />
+                  <Source
+                    symbol="[15]"
+                    source="Akhtar, Z & Rattani, A. (2017). A Face in any Form: New Challenges and Opportunities for Face Recognition Technology. Computer. 50. 80-90. 10.1109/MC.2017.119. "
+                  />
+                  <Source
+                    symbol="[16]"
+                    source="Najibi, A. (2020). Racial discrimination in face recognition technology - Science in the news. https://sitn.hms.harvard.edu/flash/2020/racial-discrimination-in-face-recognition-technology/"
+                  />
+                  <Source
+                    symbol="[17]"
+                    source="Facial recognition technology. (2024). In National Academies Press eBooks. https://doi.org/10.17226/27397"
+                  />
+                </>
+              }
               information={[
                 [
                   "Privacidad",
                   "El uso extensivo de SRF plantea preocupaciones sobre la privacidad de las personas, ya que implica la recopilación y almacenamiento de datos biométricos sensibles sin el consentimiento adecuado.",
+                  "/frs-web/assets/images/concerns/0.png",
                 ],
                 [
                   "Vulnerabilidad",
                   "Los datos faciales almacenados pueden ser vulnerables a ciberataques y accesos no autorizados, lo que podría resultar en robos de identidad o abuso de la información personal.",
+                  "/frs-web/assets/images/concerns/1.png",
                 ],
                 [
                   "Sesgos y discriminación",
                   "Los algoritmos de SRF pueden presentar sesgos raciales y de género, entre otros, lo que puede llevar a discriminación y errores en la identificación, impactando de forma negativa a ciertos grupos demográficos.",
+                  "/frs-web/assets/images/concerns/2.png",
                 ],
                 [
                   "Transparencia",
                   "A menudo las personas no son conscientes de cómo se están utilizando sus datos faciales, esto plantea problemas éticos sobre el consentimiento y la transparencia en torno al uso de esta tecnología.",
+                  "/frs-web/assets/images/concerns/3.png",
                 ],
                 [
                   "Vigilancia masiva",
                   "El uso de SRF en espacios públicos puede llevar a una vigilancia masiva, reduciendo la libertad individual creando entornos de monitoreo intrusivos y opresivos.",
+                  "/frs-web/assets/images/concerns/4.png",
                 ],
                 [
                   "Usos indebidos",
                   "Existen preocupaciones sobre el potencial uso indebido de los SRF por parte de gobiernos y empresas para fines de control social, manipulación o persecución de individuos y grupos específicos.",
+                  "/frs-web/assets/images/concerns/5.png",
                 ],
                 [
                   "Legislación y regulación insuficiente",
                   "La rápida adopción de SRF a menudo supera el desarrollo de marcos legales y regulaciones adecuadas. Estas lagunas que pueden ser explotadas a la par de generar incertidumbre sobre los derechos de las personas.",
+                  "/frs-web/assets/images/concerns/6.png",
                 ],
               ]}
             />
           </Stack>
         </Box>
-        {/* Concerns */}
-        <Box width="100%" height="calc(100vh / 3 * 2)">
-          <Stack height="100%" direction="row" spacing={0}>
+        {/* Future */}
+        <Box
+          width="100%"
+          height={{ md: "calc(100vh / 3 * 4)", lg: "calc(100vh / 3 * 2)" }}
+        >
+          <Stack
+            height="100%"
+            direction={{ base: "column", lg: "row" }}
+            spacing={0}
+          >
             <Box flex="1" height="100%" bg="timberwolf.500">
               <Center height="100%">
                 <VStack padding={10} spacing={5}>
                   <Heading
                     width="100%"
-                    fontSize={{ base: "6xl", lg: "4xl" }}
+                    fontSize={{ base: "3xl", lg: "4xl" }}
                     color="gunmetal.500"
                     textShadow="4px 4px 0px #4CAEBD"
                     textAlign="left"
@@ -339,7 +475,7 @@ const Overlay = () => {
                     width="100%"
                     textAlign="justify"
                     color="gunmetal.500"
-                    fontSize={{ base: "lg", lg: "xl" }}
+                    fontSize={{ base: "md", lg: "lg" }}
                   >
                     La integración de los SRF será cada vez más común y se
                     expandirá a nuevas industrias con diversas aplicaciones.
@@ -353,6 +489,14 @@ const Overlay = () => {
                     privacidad. La regulación y adopción responsable serán clave
                     para garantizar un uso seguro y respetuoso de los derechos
                     en un mundo hiperconectado y digitalizado.
+                    <Source
+                      symbol="[18]"
+                      source="Wright, E. (2019). The Future of Facial Recognition Is Not Fully Known: Developing Privacy and Security Regulatory Mechanisms for Facial Recognition in the Retail Sector, 29(2). https://ir.lawnet.fordham.edu/iplj/vol29/iss2/6/"
+                    />
+                    <Source
+                      symbol="[19]"
+                      source="Staffer, E. (2021). Europe’s Next Steps in Regulating Facial Recognition Technology. Columbia Journal Of Transnational Law. https://www.jtl.columbia.edu/bulletin-blog/europes-next-steps-in-regulating-facial-recognition-technology"
+                    />
                   </Text>
                 </VStack>
               </Center>
@@ -368,7 +512,12 @@ const Overlay = () => {
           </Stack>
         </Box>
         <Box width="100%" height="calc(100vh / 3 * 2)">
-          <Stack direction="row" width="100%" height="100%" spacing={0}>
+          <Stack
+            direction={{ base: "column", lg: "row" }}
+            width="100%"
+            height="100%"
+            spacing={0}
+          >
             {/* Artwork */}
             <CarouselCard
               title="Datascopio"
